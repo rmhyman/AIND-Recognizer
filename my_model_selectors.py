d6191b2154f3da67b67c0fdeae76b98c41e92984
rmhyman <ransford.hyman@gmail.com>
2017-05-23 08:03:01 -0700

    Implemented Changes up to Part 2.

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -71,13 +71,29 @@ class SelectorBIC(ModelSelector):
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
-
         :return: GaussianHMM object
         """
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        # warnings.filterwarnings("ignore", category=DeprecationWarning)
+        max_score = float('inf')
+        best_model = None
+        for n_states in range(self.min_n_components,self.max_n_components + 1):
+            model = self.base_model(n_states)
+            if model != None:
+                try:
+                    p = model.n_features
+                    N = np.sum(self.lengths)
+                    logl = model.score(self.X, self.lengths)
+                    bic = -2 * logl * p * math.log(N)
+
+                    if bic < max_score:
+                        max_score = bic
+                        best_model = model
+                except:
+                    if self.verbose:
+                        print("Not able to score for model on {} in BIC model selection.  "
+                              "Skipping model with {} hiddent states".format(self.this_word,n_states))
+        return best_model
 
 
 class SelectorDIC(ModelSelector):
@@ -89,11 +105,45 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
+
+    def compute_sum_of_likelihoods(self,model):
+        likelihoods = []
+        for key in self.hwords:
+            X, lengths = self.hwords[key]
+            likelihoods.append(model.score(X,lengths))
+        return np.sum(likelihoods)
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        """
+        M = number of training words
+        Need to compute the log-likihoods of all of the other words besides the current word
+        """
+        max_score = float('inf')
+        best_model = None
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        for n_states in range(self.min_n_components, self.max_n_components + 1):
+            model = self.base_model(n_states)
+            if model != None:
+                try:
+                    sum_of_likelihoods = self.compute_sum_of_likelihoods(model)
+                    logL = model.score(self.X,self.lengths)
+                    M = len(self.hwords)
+
+                    #To get the proper sum, we will subtract the logL from the sum_of_likelihoods value
+                    likelihood_sum_value = sum_of_likelihoods - logL
+                    DIC = logL - (1/M-1) * likelihood_sum_value
+
+                    if DIC < max_score:
+                        max_score = DIC
+                        best_model = model
+                except:
+                    if self.verbose:
+                        print("Not able to score for model on {} in DIC model selection. "
+                              "Skipping model with {} hidden states".format(self.this_word,n_states))
+
+        return best_model
+
+
 
 
 class SelectorCV(ModelSelector):
@@ -103,6 +153,26 @@ class SelectorCV(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        best_avg = float("-inf")
+        best_model = None
+        for n_states in range(self.min_n_components, self.max_n_components + 1):
+            logl_values = []
+            model = self.base_model(n_states)
+            if model != None:
+                try:
+                    split_method = KFold()
+                    for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                        X,lengths = combine_sequences(cv_train_idx,self.sequences)
+                        logl_values.append(model.score(X,lengths))
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+                    avg_log = np.mean(logl_values)
+                    if avg_log > best_avg:
+                        best_avg = avg_log
+                        best_model = model
+                except:
+                    if best_model == None:
+                        best_model = model
+                    if self.verbose:
+                        print("Not able to score for model on {} in CV model selection. "
+                                "Skipping model with {} hidden states".format(self.this_word,n_states))
+        return best_model
